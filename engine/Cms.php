39f0aa9cece6a52fe6f6010386eda2b80004ece7
Dok2314 <koversundanil@gmail.com>
2022-08-16 19:40:05 +0300

    Added bootstraping

---
+++ engine/Cms.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace engine;
+
+class Cms
+{
+    /**
+     * @var
+     */
+    private $di;
+
+    /**
+     * @param $di
+     */
+    public function __construct($di)
+    {
+        $this->di = $di;
+    }
+
+    /**
+     * run CMS
+     */
+    public function run()
+    {
+
+    }
+}
--- engine/bootstrap.php
+++ engine/bootstrap.php
@@ -1,1 +1,16 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use engine\Cms;
+use engine\di\DI;
+
+try {
+    // Dependency injection
+    $di = new DI();
+
+    $cms = new Cms($di);
+    $cms->run();
+}catch (\ErrorException $e) {
+    echo $e->getMessage();
+}
