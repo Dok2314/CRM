94f8fb7fced0449d6e17298dd96c20727640f2cf
Dok2314 <koversundanil@gmail.com>
2022-08-20 17:02:51 +0300

    Added router functionality and dispatch

--- engine/Core/Router/UrlDispatcher.php
+++ engine/Core/Router/UrlDispatcher.php
@@ -39,17 +39,55 @@ class UrlDispatcher
         $this->patterns[$key] = $pattern;
     }
 
+    /**
+     * @param $method
+     * @return array|mixed|null
+     */
     private function routes($method)
     {
         return $this->routes[$method] ?? null;
     }
 
+    /**
+     * @param $method
+     * @param $pattern
+     * @param $controller
+     * @return void
+     */
+    public function register($method, $pattern, $controller)
+    {
+        $this->routes[strtoupper($method)][$pattern] = $controller;
+    }
+
+    /**
+     * @param $method
+     * @param $uri
+     * @return DispatchedRoute|void
+     */
     public function dispatch($method, $uri)
     {
         $routes = $this->routes(strtoupper($method));
 
         if(array_key_exists($uri, $routes)) {
             return new DispatchedRoute($routes[$uri]);
+        }
+
+        return $this->doDispatch($method, $uri);
+    }
+
+    /**
+     * @param $method
+     * @param $uri
+     * @return void
+     */
+    private function doDispatch($method, $uri)
+    {
+        foreach ($this->routes($method) as $route => $controller) {
+            $pattern = '#^' . $route . '$#s';
+
+            if(preg_match($pattern, $uri, $parameters)) {
+                return new DispatchedRoute($controller, $parameters);
+            }
         }
     }
 }
